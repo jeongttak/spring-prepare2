2c1bc666a6157577b376e370f327f421cc6f0ddd
jeongttak <okokyong1@gmail.com>
2025-07-02 11:48:21 +0900

    first commit

---
+++ src/test/java/com/metaverse/springprepare2/SpringPrepare2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.springprepare2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepare2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
