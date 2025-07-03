056efa81aa43ad8e4a069b592b6015c79b92b28b
jeongttak <okokyong1@gmail.com>
2025-07-03 15:28:03 +0900

    test : Junit을 활용한 테스트 코드 기능 추가

--- src/test/java/com/metaverse/springprepare2/testsample/CalculatorTest.java
+++ src/test/java/com/metaverse/springprepare2/testsample/CalculatorTest.java
@@ -10,19 +10,79 @@ class CalculatorTest {
     @Test
     @DisplayName("더하기 테스트")
     void test1() {
+        // given 테스트에 사용될 값들
         Calculator calculator = new Calculator();
-        Double result = calculator.operate(8, "+", 2);
+        int num1 = 8;
+        int num2 = 2;
+        String op = "+";
+        //when 테스트 실행
+        Double result = calculator.operate(num1, op, num2);
+        //then 결과
         System.out.println("result = " + result);
 
         Assertions.assertEquals(10, result);
     }
+
     @Test
     @DisplayName("나눗셈 테스트")
     void test2() {
         Calculator calculator = new Calculator();
-        Double result = calculator.operate(8, "/", 2);
+        int num1 = 8;
+        int num2 = 2;
+        String op = "/";
+        Double result = calculator.operate(num1, op, num2);
         System.out.println("result = " + result);
 
         Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    @DisplayName("곱하기 테스트")
+    void test3() {
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 2;
+        String op = "*";
+        Double result = calculator.operate(num1, op, num2);
+
+        System.out.println("result = " + result);
+        Assertions.assertEquals(16, result);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void test4() {
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 2;
+        String op = "-";
+        Double result = calculator.operate(num1, op, num2);
+        System.out.println("result = " + result);
+        Assertions.assertEquals(6, result);
+    }
+    @Test
+    @DisplayName("예외 테스트")
+    void test5() {
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 2;
+        String op = "%";
+
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.operate(num1, op, num2)
+        );
+
+        Assertions.assertEquals("잘못된 연산자 입니다.", thrown.getMessage());
+    }
+    @Test
+    @DisplayName("NULL 테스트")
+    void test6() {
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 0; //null
+        String op = "/";
+        Double result = calculator.operate(num1, op, num2);
+        System.out.println("result = " + result);
+        Assertions.assertNull(result);
     }
 }
