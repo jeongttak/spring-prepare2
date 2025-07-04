336456e2d08fafee2d644b05cd4138ae5ce4ada4
jeongttak <okokyong1@gmail.com>
2025-07-04 10:32:46 +0900

    feat: Lombok 에너테이션 활용 2
    * @NoArgsConstructor 기본 생성자 애너테이션
    * @AllArgsConstructor 모든 필드를 받는 애너테이션

--- src/main/java/com/metaverse/springprepare2/domain/Memo.java
+++ src/main/java/com/metaverse/springprepare2/domain/Memo.java
@@ -1,10 +1,14 @@
 package com.metaverse.springprepare2.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
+@NoArgsConstructor//기본생성자 생성 애너테이션
+@AllArgsConstructor //모든 필드를 파라미터로 받는 생성자
 public class Memo {
     private String username;
     private String content;
